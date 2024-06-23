ca351a8ffdb6405a063dcfa89181e22f6b4cb766
quickjavahelp <quickjavahelp@gmail.com>
2024-06-23 13:03:40 +0530

    ommiting all updated

--- VidyalayaWebFront/src/app/app.component.ts
+++ VidyalayaWebFront/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SetupWindowComponent } from './setup/setup-window/setup-window.component';
+import { MainheaderComponent } from './common/mainheader/mainheader.component';
+import { MainfooterComponent } from './common/mainfooter/mainfooter.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MainheaderComponent,MainfooterComponent,SetupWindowComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
