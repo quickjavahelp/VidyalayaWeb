8426bb86794dd7c9085d2c145e50a680c02d1ad6
quickjavahelp <quickjavahelp@gmail.com>
2024-07-02 06:31:36 +0530

    New Code

--- VidyalayaWebFront/src/app/common/address/address.component.ts
+++ VidyalayaWebFront/src/app/common/address/address.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, Input, OnInit, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -6,26 +6,38 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { SchoolSetupServiceService } from '../../setup/schoolsetup/school-setup-service.service';
 import { Country } from '../../model/country.model';
+import { State } from '../../model/state.model';
+import { ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-address',
   standalone: true,
-  imports: [MatExpansionModule,MatInputModule,MatSelectModule,MatButtonModule,MatDividerModule],
-  providers:[SchoolSetupServiceService],
+  imports: [MatExpansionModule,MatInputModule,MatSelectModule,MatButtonModule,MatDividerModule, ReactiveFormsModule],
+  providers:[SchoolSetupServiceService, Input],
   templateUrl: './address.component.html',
   styleUrl: './address.component.css'
 })
+
 export class AddressComponent implements OnInit{
-  public cou:Country | undefined;
-  public country: Array<Country> = [];
+  public cou!: Country;
+  public countrys: Array<Country> = [];
+  public states: Array<State> = [];
+
+  @Input() sufixIdFromPArent!: String;
+  
   readonly panelOpenState = signal(false);
   public schoolSetupService: SchoolSetupServiceService;
   constructor(schoolSetupService:SchoolSetupServiceService){
-    this.schoolSetupService=schoolSetupService;
+    this.schoolSetupService = schoolSetupService;
   }
 
   ngOnInit(): void {
-    this.country = this.schoolSetupService.getCountryList();
+    this.countrys = this.schoolSetupService.getCountryList();   
+  }
+
+  public getState(){
+    debugger;
+    this.states = this.schoolSetupService.getStateList(this.cou);
   }
 
 }
--- VidyalayaWebFront/src/app/model/state.model.ts
+++ VidyalayaWebFront/src/app/model/state.model.ts
@@ -11,4 +11,19 @@ public created_by: String = "";
 public created_date: String = ""; 
 public updated_by: String = ""; 
 public updated_date: String = ""; 
+
+constructor(state:State){
+    this.uid= state.uid;
+    this.state_name= state.state_name; 
+    this.code= state.code; 
+    this.capital= state.capital; 
+    this.country_code= state.country_code; 
+    this.json_details= state.json_details; 
+    this.is_deleted= state.is_deleted; 
+    this.is_active= state.is_active; 
+    this.created_by= state.created_by; 
+    this.created_date= state.created_date; 
+    this.updated_by= state.updated_by; 
+    this.updated_date= state.updated_date;
+}
 }
